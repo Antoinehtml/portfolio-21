054d6da6a5200ab4c29257f33680fa8130f9441f
Antoine.HTML <76158618+Antoinehtml@users.noreply.github.com>
2021-09-10 00:45:16 +0200

    added text and animation to text

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,5 +1,8 @@
 gsap.from('.right', {duration: 10, opacity: 0, ease:"power1"})
 
+gsap.from('.img' , {duration: 2, opacity: 0, x: -300, delay: 2, ease:"circ"})
+gsap.from('.h3' , {duration: 2, opacity: 0, y: -300, delay: 3, ease:"circ"})
+gsap.from('.p' , {duration: 10, opacity: 0, delay: 3, ease:"power1"})
 
 const circleSvg = document.querySelector('svg');
 
