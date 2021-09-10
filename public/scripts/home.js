85ef88e30e6d3c8113cdde05423368829fc4e0ca
Antoine.HTML <76158618+Antoinehtml@users.noreply.github.com>
2021-09-10 10:59:57 +0200

    changed design of text and animated with scroll

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,8 +1,55 @@
-gsap.from('.right', {duration: 10, opacity: 0, ease:"power1"})
+gsap.from('.right', {duration: 6, opacity: 0, ease:"power1"})
 
-gsap.from('.img' , {duration: 2, opacity: 0, x: -300, delay: 2, ease:"circ"})
-gsap.from('.h3' , {duration: 2, opacity: 0, y: -300, delay: 3, ease:"circ"})
-gsap.from('.p' , {duration: 10, opacity: 0, delay: 3, ease:"power1"})
+
+gsap.from(".image" , {
+	scrollTrigger: {
+		trigger: '.about',
+		start: "top center"
+	},
+	x: -150,
+	opacity: 0,
+	duration: 2
+})
+
+gsap.from(".antoine" , {
+	scrollTrigger: {
+		trigger: '.about',
+		start: "top center"
+	},
+	y: -150,
+	opacity: 0,
+	duration: 3
+})
+
+gsap.from(".p" , {
+	scrollTrigger: {
+		trigger: '.about',
+		start: "top center"
+	},
+	x: 150,
+	opacity: 0,
+	duration: 3
+})
+
+
+
+// gsap.to(".square", {
+// 	scrollTrigger: {
+// 		trigger: '.sq',
+// 		start: "top centre"
+// 	},
+// 	x: 500
+// })
+
+// gsap.from('.pic' , {duration: 2, opacity: 0, x: -300, delay: 2, ease:"circ"})
+// gsap.from('.h3' , {duration: 2, opacity: 0, y: -300, delay: 3, ease:"circ"})
+// gsap.from('.p' , {duration: 10, opacity: 0, delay: 3, ease:"power1"})
+
+
+
+
+
+
 
 const circleSvg = document.querySelector('svg');
 
